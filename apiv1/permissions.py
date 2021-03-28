0c696808f936818adada6ad811a9f1ed93e8f153
dyachkov-pavel <pahanthebest12345@mail.ru>
2021-03-28 18:37:27 +0300

    pep2

--- apiv1/permissions.py
+++ apiv1/permissions.py
@@ -16,18 +16,20 @@ class IsSafeOrAdminPermission(permissions.BasePermission):
     """
     GET для всех иначе только администраторы.
     """
+
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-                or (request.user.is_authenticated 
-                and request.user.is_admin))
+                or (request.user.is_authenticated
+                    and request.user.is_admin))
 
 
 class IsAuthorOrAbovePermission(permissions.BasePermission):
     """
     POST и 'SAFE' для всех, иначе только автор, модераторы и админы.
     """
+
     def has_object_permission(self, request, view, obj):
-        return (request.method not in ('PUT', 'PATCH', "DELETE") or
+        return (request.method not in ('PUT', 'PATCH', "DELETE")
                 or request.user == obj.author
                 or (request.user.is_authenticated
                 and request.user.is_moderator))
